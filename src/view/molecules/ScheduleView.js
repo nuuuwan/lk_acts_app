f4849f982fe7ee8e78d8137e884b1eeeb7362ebf
nuuuwan <nuuuwan@gmail.com>
2022-11-16 17:06:32 +0530

    feat: Added schedule content

--- src/view/molecules/ScheduleView.js
+++ src/view/molecules/ScheduleView.js
@@ -1,4 +1,22 @@
+import Box from "@mui/material/Box";
 import LinesView from "../../view/molecules/LinesView";
+import SectionView from "../../view/molecules/SectionView";
+
+const STYLE = {
+  color: 'black',
+  fontSize: '100%',
+  fontWeight: "bold",
+}
+
 export default function ScheduleView({ schedule }) {
-  return <LinesView lines={schedule.textlines} variant="h6" />;
+  return (
+    <Box>
+      <LinesView lines={schedule.textlines} sx={STYLE}/>
+      {schedule.sections.map(
+        function(section, iSection) {
+          return <SectionView key={'section-' + iSection} section={section} />;
+        }
+      )}
+    </Box>
+  );
 }

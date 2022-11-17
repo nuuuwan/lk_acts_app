517a813553eef9d1b90e8b3b62d9b8ea401ac0e9
nuuuwan <nuuuwan@gmail.com>
2022-11-17 15:24:26 +0530

    feat: use LineView

--- src/view/molecules/ActTitleView.js
+++ src/view/molecules/ActTitleView.js
@@ -1,10 +1,11 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import LineView from "../../view/molecules/LineView";
 
 export default function ActTitleView({ act }) {
   return (
     <Box>
-      <Typography variant="h4">{act.shortTitle}</Typography>
+      <LineView line={act.shortTitle} sx={{fontSize: "200%"}} />
       <Typography variant="h6">
         Act No. {act.num} of {act.year}
       </Typography>

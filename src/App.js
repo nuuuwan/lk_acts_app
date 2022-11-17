3bd9bfd1205ba2a27e739d843fd0d9e4fb83c906
nuuuwan <nuuuwan@gmail.com>
2022-11-17 11:03:55 +0530

    style: Updated fonts; titles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-import HomePage from "./view/pages/HomePage";
+import React, { Component } from "react";
 
-export default function App() {
-  return <HomePage />;
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import HomePage from "./view/pages/HomePage.js";
+
+const THEME = createTheme({
+  palette: {
+    primary: {
+      main: "#000",
+    },
+    secondary: {
+      main: "#ccc",
+    },
+  },
+  typography: {
+    fontFamily: ["Figtree", "sans-serif"].join(","),
+    fontSize: 14,
+  },
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={THEME}>
+        <HomePage />
+      </ThemeProvider>
+    );
+  }
 }

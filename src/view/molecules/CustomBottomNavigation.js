3d1b9ac556f7a4d8f9ca68c8bc54c80ff179fade
nuuuwan <nuuuwan@gmail.com>
2022-11-17 15:41:01 +0530

    feat: Added CopyLink

--- src/view/molecules/CustomBottomNavigation.js
+++ src/view/molecules/CustomBottomNavigation.js
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import RefreshIcon from "@mui/icons-material/Refresh";
 
 const STYLE = {
@@ -12,14 +13,24 @@ const STYLE = {
   height: 72,
 };
 
+const URL_APP = "https://nuuuwan.github.io/lk_acts_app/";
+
 export default function CustomBottomNavigation() {
   const onClickRefresh = function () {
     localStorage.clear();
     window.location.reload();
   };
+
+  const onClickCopyLink = function () {
+    navigator.clipboard.writeText(URL_APP);
+  };
   return (
     <BottomNavigation sx={STYLE}>
       <BottomNavigationAction icon={<RefreshIcon />} onClick={onClickRefresh} />
+      <BottomNavigationAction
+        icon={<ContentCopyIcon />}
+        onClick={onClickCopyLink}
+      />
     </BottomNavigation>
   );
 }

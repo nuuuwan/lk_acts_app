015d7741a7133392b7bd6348a1f0f306901b3a72
nuuuwan <nuuuwan@gmail.com>
2022-11-17 14:18:42 +0530

    feat: Removed "The Democratic Socialist Republic of Sri Lanka"

--- src/view/molecules/ActTitleView.js
+++ src/view/molecules/ActTitleView.js
@@ -4,9 +4,6 @@ import Typography from "@mui/material/Typography";
 export default function ActTitleView({ act }) {
   return (
     <Box>
-      <Typography variant="body1">
-        The Democratic Socialist Republic of Sri Lanka
-      </Typography>
       <Typography variant="h4">{act.shortTitle}</Typography>
       <Typography variant="h6">
         Act No. {act.num} of {act.year}

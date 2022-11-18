73a435e71484fd51a27733eabe3628bb29b278f1
nuuuwan <nuuuwan@gmail.com>
2022-11-18 10:36:45 +0530

    style: Fonts even smaller

--- src/view/molecules/ActTitleView.js
+++ src/view/molecules/ActTitleView.js
@@ -6,8 +6,8 @@ import LineView from "../../view/molecules/LineView";
 export default function ActTitleView({ act }) {
   return (
     <Box>
-      <LineView line={act.shortTitle} sx={{ fontSize: "150%" }} />
-      <Typography variant="body1">
+      <LineView line={act.shortTitle} sx={{ fontSize: "75%" }} />
+      <Typography sx={{ fontSize: "50%" }}>
         Act No. {act.num} of {act.year}
       </Typography>
     </Box>

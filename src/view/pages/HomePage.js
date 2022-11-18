c78b73f526873c0e3895e92b8050560bee4ace90
nuuuwan <nuuuwan@gmail.com>
2022-11-18 10:27:18 +0530

    fix: Fixed scrolling BUG

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -9,6 +9,16 @@ import ActEntityView from "../../view/molecules/ActEntityView";
 import ActIndexView from "../../view/molecules/ActIndexView";
 import CustomBottomNavigation from "../../view/molecules/CustomBottomNavigation";
 
+const STYLE_BOX_INNER = {
+  position: "fixed",
+  bottom: 72,
+  left: 0,
+  right: 0,
+  top: 0,
+  overflow: "scroll",
+  padding: 3,
+};
+
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
@@ -21,7 +31,10 @@ export default class HomePage extends Component {
   }
 
   onClickEntity(entity) {
-    console.debug("onClickEntity", entity);
+    const boxInner = document.getElementById("box-inner");
+    console.debug(boxInner);
+    boxInner.scrollTop = 0;
+
     this.setState({ activeEntity: entity });
   }
 
@@ -44,7 +57,9 @@ export default class HomePage extends Component {
   render() {
     return (
       <Box>
-        {this.renderInner()}
+        <Box sx={STYLE_BOX_INNER} id="box-inner">
+          {this.renderInner()}
+        </Box>
         <CustomBottomNavigation onClickIndex={this.onClickIndex.bind(this)} />
       </Box>
     );

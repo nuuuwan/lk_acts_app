549b5a17c6f26fa3b06f39af6c4fc42254949ae9
nuuuwan <nuuuwan@gmail.com>
2022-11-19 10:13:23 +0530

    feat: Added pause for Speaker

--- src/view/molecules/LineView.js
+++ src/view/molecules/LineView.js
@@ -1,19 +1,29 @@
+import React, { useState } from "react";
 import { useSpeechSynthesis } from "react-speech-kit";
 
 import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import PauseIcon from "@mui/icons-material/Pause";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 const MIN_TEXT_FOR_SPEAKER = 30;
 
 export default function LineView({ line, sx }) {
+  const [isSpeaking, setIsSpeaking] = useState(false);
+
   const showSpeaker = line.length > MIN_TEXT_FOR_SPEAKER;
-  const { speak } = useSpeechSynthesis();
+  const { speak, cancel } = useSpeechSynthesis();
 
   const onClick = function (event) {
     event.stopPropagation();
-    speak({ text: line });
+    if (isSpeaking) {
+      setIsSpeaking(false);
+      cancel();
+    } else {
+      setIsSpeaking(true);
+      speak({ text: line });
+    }
   };
 
   const styleIcon = { color: sx.color, opacity: 0.2 };
@@ -24,7 +34,11 @@ export default function LineView({ line, sx }) {
       </Typography>
       {showSpeaker ? (
         <IconButton onClick={onClick} size="small">
-          <PlayArrowIcon sx={styleIcon} />
+          {isSpeaking ? (
+            <PauseIcon sx={styleIcon} />
+          ) : (
+            <PlayArrowIcon sx={styleIcon} />
+          )}
         </IconButton>
       ) : null}
     </Stack>

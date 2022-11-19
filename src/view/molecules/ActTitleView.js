ef44df424808aa09936762d63c5d37786f79ca7f
nuuuwan <nuuuwan@gmail.com>
2022-11-19 09:42:41 +0530

    hack: onClickAvatar

--- src/view/molecules/ActTitleView.js
+++ src/view/molecules/ActTitleView.js
@@ -5,6 +5,11 @@ import Typography from "@mui/material/Typography";
 
 import LineView from "../../view/molecules/LineView";
 
+function onClickAvatar() {
+  localStorage.clear();
+  window.location.reload();
+}
+
 export default function ActTitleView({ act }) {
   return (
     <Stack direction="row" spacing={2}>
@@ -12,6 +17,7 @@ export default function ActTitleView({ act }) {
         alt="Logo"
         src={`${process.env.PUBLIC_URL}/logo512.png`}
         sx={{ height: 32, width: 32 }}
+        onClick={onClickAvatar}
       />
       <Box>
         <LineView line={act.shortTitle} sx={{ fontSize: "75%" }} />

76f31bbc8fdb4b847bd86f67fc221dca6b2f2a75
nuuuwan <nuuuwan@gmail.com>
2022-11-19 08:50:24 +0530

    feat: + ENTITY_TYPES

---
+++ src/nonview/core/ENTITY_TYPES.js
@@ -0,0 +1,10 @@
+const ENTITY_TYPES = {
+  L0Part: "L0Part",
+  L0Schedule: "L0Schedule",
+  L1Section: "L1Section",
+  L2SubSection: "L2SubSection",
+  L3Paragraph: "L3Paragraph",
+  L4SubParagraph: "L4SubParagraph",
+};
+
+export default ENTITY_TYPES;
